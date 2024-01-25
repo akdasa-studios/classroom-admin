8ea50a1f8d533bac7864b488a2d7b84164c9f8a8
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2024-01-25 11:47:41 +0000

    Add getRole method to RolesService

--- src/org/services/RolesService.ts
+++ src/org/services/RolesService.ts
@@ -1,14 +1,18 @@
 import { UuidIdentity } from "@akd-studios/framework/domain"
-import { Role } from "@core/aggregates"
+import { Role, type RoleIdentity } from "@core/aggregates"
 import { Permission } from "@core/aggregates/permissions"
 
 export class RolesService {
+  async getRole(roleId: RoleIdentity): Promise<Role> {
+    return new Role(new UuidIdentity(roleId.value), "Role X", "Description", [Permission.OrgRolesManage])
+  }
+
   async getAllRoles(): Promise<Role[]> {
     return [
-      new Role(new UuidIdentity(), "Role", "Description", [Permission.OrgRolesManage]),
-      new Role(new UuidIdentity(), "Role", "Description", [Permission.OrgRolesManage]),
-      new Role(new UuidIdentity(), "Role", "Description", [Permission.OrgRolesManage]),
-      new Role(new UuidIdentity(), "Role", "Description", [Permission.OrgRolesManage]),
+      new Role(new UuidIdentity(), "Role 1", "Description", [Permission.OrgRolesManage]),
+      new Role(new UuidIdentity(), "Role 2", "Description", [Permission.OrgRolesManage]),
+      new Role(new UuidIdentity(), "Role 3", "Description", [Permission.OrgRolesManage]),
+      new Role(new UuidIdentity(), "Role 4", "Description", [Permission.OrgRolesManage]),
     ]
   }
 }
