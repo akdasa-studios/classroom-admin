bdcfbf61fe27abd4afe293e82026259053c2697f
Advaita Kṛṣṇa Dāsa <advaita.krishna.das@gmail.com>
2024-01-26 15:05:08 +0000

    Refactor Table component styling

--- src/shared/components/Kit/Table/Table.vue
+++ src/shared/components/Kit/Table/Table.vue
@@ -1,20 +1,24 @@
 <template>
-  <table class="min-w-full divide-y divide-gray-300">
-    <thead>
-      <slot name="head" />
-    </thead>
-    <tbody class="divide-y divide-gray-200 bg-white">
-      <slot />
-    </tbody>
-  </table>
+  <div class="Table">
+    <table class="min-w-full divide-y divide-gray-300 bg-gray-50">
+      <thead>
+        <slot name="head" />
+      </thead>
+      <tbody class="divide-y divide-gray-200 bg-white">
+        <slot />
+      </tbody>
+    </table>
+  </div>
 </template>
 
 
-
-<style scoped>
+<style scoped lang="scss">
 .Table {
-  border-top-width: 1px;
-  border-color: #D1D5DB;
-  min-width: 100%;
+  overflow: hidden;
+  box-shadow: var(--tw-ring-inset) 0 0 0 calc(1px + var(--tw-ring-offset-width)) var(--tw-ring-color);
+  --ring-color: #000000;
+  --ring-opacity: 0.05;
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+  border-radius: 0.25rem;
 }
 </style>
